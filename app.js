5c646619f5d04996a61ee317a4afa5e20b2cc1d0
Mitchell Van Der Hoeff <mitchellvanderhoeff@gmail.com>
2016-06-08 14:58:55 -0400

    Goddammit Simon

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ app.use(session({ secret: process.env.SESSION_SECRET }));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(routes);
 app.use(ddos.express);
+app.use(routes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
